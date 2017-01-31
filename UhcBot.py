0a925a0ddd625e4a75ea34627791eab12ca8a66d
LotU <j.koopman@jarict.nl>
2017-01-31 10:11:35 +0100

    Create UhcBot.py

---
+++ UhcBot.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------#
+#     "#" signs at the beginning of lines are comments these lines do not #
+#     affect how to code is read by Python, I would highly reccomend to   #
+#     comment the potato out of the code so it is as easy to read as      #
+#          possible, (it doesn't need to be as formatted as mine)         #
+#-------------------------------------------------------------------------#
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+#               Message Prefix ----v
+    if message.content.startswith('!'):
+        command, *args = message.content[1:].split()
+#       !test command
+        if command == "test":
+            await client.send_message(message.channel, "Yup things seem to working... for now")
+#       End Command
+
+
+#    DO NOT LEAVE THE TOKEN IN HERE FROM NOW ON
+#    (Bad things could happen if it is public)
+client.run('TOKEN')
