d2d9ce471ec8a59babcc7896a8599c44fdd19493
Jari Koopman <MBJ@MacBook-Pro-van-Jari.local>
2017-02-20 11:34:44 +0100

    Added Config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------------------#
+#     "#" signs at the beginning of lines are comments these lines do not #
+#     affect how to code is read by Python, I would highly reccomend to   #
+#     comment the potato out of the code so it is as easy to read as      #
+#          possible, (it doesn't need to be as formatted as mine)         #
+#-------------------------------------------------------------------------#
+
+roleToAssign = "277862726594854923"
+modRole = "Moderator"
+adminRole = "Admin"
