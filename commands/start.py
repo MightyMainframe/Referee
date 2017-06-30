24dfcbd505ce469afcd000ee9b2fe66ad78f8d67
Alkali-Metal <Alkali.maps@gmail.com>
2017-06-29 18:23:03 -0600

    async-ify the function.

--- commands/start.py
+++ commands/start.py
@@ -1,11 +1,10 @@
 import discord
 
-def run(client, message, roles, *args):
+async def run(client, message, roles, *args):
     user_roles = []
     for role in message.author.roles:
         user_roles.append(role.name)
     if config["modRole"] in user_roles or config["adminRole"] in user_roles:
-        await client.change_presence(
-            game=discord.Game(name='a UHC!'))
+        await client.change_presence(game=discord.Game(name='a UHC!'))
         await client.send_message(message.channel,
                                   "Let's get to the dieing already!")
