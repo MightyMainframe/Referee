ef7c4928dda65f9a51ffb854a8ad65690f5110dd
Alkali-Metal <Alkali.maps@gmail.com>
2017-06-29 18:11:14 -0600

    Added permissions checking because /shrug

--- commands/start.py
+++ commands/start.py
@@ -1,7 +1,11 @@
 import discord
 
 def run(client, message, roles, *args):
-    await client.change_presence(
-        game=discord.Game(name='a UHC!'))
-    await client.send_message(message.channel,
-                              "Let's get to the dieing already!")
+    user_roles = []
+    for role in message.author.roles:
+        user_roles.append(role.name)
+    if config["modRole"] in user_roles or config["adminRole"] in user_roles:
+        await client.change_presence(
+            game=discord.Game(name='a UHC!'))
+        await client.send_message(message.channel,
+                                  "Let's get to the dieing already!")
