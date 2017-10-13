bc37872052cc24a706650b80c87b937d417ff5e4
MrLotU <j.koopman@jarict.nl>
2017-10-13 16:17:44 +0200

    ALL THE CLEANUP!

--- constants.py
+++ constants.py
@@ -2,10 +2,24 @@
 Constants for usage throughout the entire bot
 If you plan to self host this bot, edit them to contain values from your server
 """
+from collections import OrderedDict
+
+GAME_ADD_STEPS = {
+    'validate': 'This will start the proces of {}. Is this correct?'.format(
+        'creating a new game',
+    ),
+    'complete': 'Created game `{}`, with description `{}`. I `will{}` create channels for it.',
+    'fail': 'Process canceled. Restart it by using `!game add`'
+}
 
 PLAYING_STATUS = 'Managing the <name>'
 GLOBAL_ADMINS = [188918216008007680, 134146475591467008, 125793782665969664]
 PY_CODE_BLOCK = u'```py\n{}\n```'
+INFO_STEPS = OrderedDict([
+    ('get_name', 'Okay, please give the name of the new game'),
+    ('get_desc', 'Okay, created the game. now, give a description for the game'),
+    ('should_create_channels', 'Should I create channels for this game?')
+])
 
 def check_global_admin(uid):
     """Checks if user is global admin and thus can use all commands"""
--- plugins/game.py
+++ plugins/game.py
@@ -1,55 +1,57 @@
 from disco.bot import Plugin
 import gevent
-from constants import check_global_admin
+from constants import check_global_admin, GAME_ADD_STEPS, INFO_STEPS
 
 class GameManager(Plugin):
     @Plugin.command('add', group='game')
     def add_command(self, event):
+        """This is one big heck of a function"""
         if not check_global_admin(event.msg.author.id):
             return
 
         messages_to_delete = []
 
         def remove_messages():
+            """If process is canceled, remove all messages marked for deletion"""
             for message in messages_to_delete:
-                event.msg.channel.get_message(message).delete()
+                message.delete()
 
         msg = event.msg
-        confirm_msg = msg.reply('This will start the proces of {}. Is this correct?'.format(
-            'creating a new game',
+        confirm_msg = msg.reply(GAME_ADD_STEPS['validate'])
+        messages_to_delete.append(msg)
+        messages_to_delete.append(confirm_msg)
+        try:
+            messages_to_delete.append(self.wait_for_event(
+                'MessageCreate',
+                conditional=lambda e: (
+                    e.message.content.lower().startswith('y') and e.message.author == msg.author
+                )
+            ).get(timeout=30))
+        except gevent.Timeout:
+            messages_to_delete.append(msg.reply(GAME_ADD_STEPS['fail']))
+            remove_messages()
+            return
+        for key, value in INFO_STEPS.iteritems():
+            messages_to_delete.append(msg.reply(value))
+            try:
+                message = self.wait_for_event(
+                    'MessageCreate',
+                    conditional=lambda e: (
+                        e.message.author == msg.author
+                    )
+                ).get(timeout=30)
+            except gevent.Timeout:
+                messages_to_delete.append(msg.reply(GAME_ADD_STEPS['fail']))
+                remove_messages()
+                return
+            if key == 'get_name':
+                game_name = message.content
+            elif key == 'get_desc':
+                game_desc = message.content
+            else:
+                create_channels = bool(message.content.lower().startswith('y'))
+        msg.reply('{}'.format(
+            GAME_ADD_STEPS['complete'].format(
+                game_name, game_desc, ('' if create_channels is True else ' not')
+            )
         ))
-        messages_to_delete.append(msg.id)
-        messages_to_delete.append(confirm_msg.id)
-        try:
-            message = self.wait_for_event(
-                'MessageCreate',
-                conditional=lambda e: (
-                    (e.message.content.lower() == 'y' or e.message.content.lower() == 'yes') and 
-                    e.message.author == msg.author
-                )
-            ).get(timeout=30)
-            messages_to_delete.append(message.id)
-        except gevent.Timeout:
-            fail_msg = msg.reply('Request timed out!')
-            messages_to_delete.append(fail_msg.id)
-            remove_messages()
-            return
-        
-        give_name = msg.reply('Okay, please give the name of the new game')
-        messages_to_delete.append(give_name.id)
-        try:
-            game_name_msg = self.wait_for_event(
-                'MessageCreate',
-                conditional=lambda e: (
-                    e.message.author == msg.author
-                )
-            ).get(timeout=30)
-            messages_to_delete.append(game_name_msg.id)
-        except gevent.Timeout:
-            fail_msg = msg.reply('Request timed out!')
-            messages_to_delete.append(fail_msg.id)
-            remove_messages()
-            return
-        game_name = game_name_msg.content
-        print game_name
-        msg.reply('Wew!')
