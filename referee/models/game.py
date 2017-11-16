09911bfd1039df666ef4652e71b9f1d536343121
MrLotU <j.koopman@jarict.nl>
2017-11-16 13:44:47 +0100

    [game] Base setup for interactive joining

--- referee/models/game.py
+++ referee/models/game.py
@@ -1,8 +1,11 @@
+import pprint
 from datetime import datetime
 
 from peewee import (BigIntegerField, BooleanField, CharField, DateTimeField,
                     IntegerField, SmallIntegerField, TextField)
+from playhouse.postgres_ext import ArrayField, JSONField
 
+from referee.constants import PY_CODE_BLOCK
 from referee.db import BaseModel
 from referee.util.input import parse_duration
 
@@ -17,9 +20,39 @@ class Game(BaseModel):
     next_announcement = DateTimeField(null=True)
     last_announcement = DateTimeField()
     interval = CharField(null=True)
+    attendees = ArrayField(BigIntegerField)
+    join_src = TextField(null=True)
+    play_src = TextField(null=True)
 
     class Meta:
         db_table = 'games'
+
+    def execute_join(self, event):
+        ctx = {
+            'event': event,
+            'msg': event.msg,
+            'guild': event.msg.guild,
+            'channel': event.msg.channel,
+            'author': event.msg.author
+        }
+        src = '{}'.format(self.join_src)
+        lines = filter(bool, src.split('\n'))
+        if lines[-1] and 'return' not in lines[-1]:
+            lines[-1] = 'return ' + lines[-1]
+        lines = '\n'.join('    ' + i for i in lines)
+        code = 'def f():\n{}\nx = f()'.format(lines)
+        local = {}
+        try:
+            exec compile(code, '<eval>', 'exec') in ctx, local
+        except Exception as e:
+            event.msg.reply(PY_CODE_BLOCK.format(type(e).__name__ + ': ' + str(e)))
+            return
+        result = pprint.pformat(local['x'])
+
+        if len(result) > 1990:
+            event.msg.reply('', attachments=[('result.txt', result)])
+        else:
+            event.msg.reply(PY_CODE_BLOCK.format(result))
 
     def set_announcement_channel(self, a_channel):
         query = Game.update(a_channel=a_channel)
@@ -50,7 +83,10 @@ class Game(BaseModel):
             a_message=None,
             interval=None,
             last_announcement=datetime.utcnow(),
-            next_announcement=None
+            next_announcement=None,
+            attendees=[],
+            join_src=None,
+            play_src=None
         )
 
     @classmethod
