11291f61ced23d3d76780561c8adcdfd86be7a67
MrLotU <j.koopman@jarict.nl>
2018-01-22 16:44:17 +0100

    More static stuff!

--- referee/static/build/js/custom.js
+++ referee/static/build/js/custom.js
@@ -1833,39 +1833,7 @@ if (typeof NProgress != 'undefined') {
         return false;
 		});
 	  
-	  };
-	   
-	  	/* PNotify */
-			
-		function init_PNotify() {
-			
-			if( typeof (PNotify) === 'undefined'){ return; }
-			console.log('init_PNotify');
-			
-			new PNotify({
-			  title: "PNotify",
-			  type: "info",
-			  text: "Welcome. Try hovering over me. You can click things behind me, because I'm non-blocking.",
-			  nonblock: {
-				  nonblock: true
-			  },
-			  addclass: 'dark',
-			  styling: 'bootstrap3',
-			  hide: false,
-			  before_close: function(PNotify) {
-				PNotify.update({
-				  title: PNotify.options.title + " - Enjoy your Stay",
-				  before_close: null
-				});
-
-				PNotify.queueRemove();
-
-				return false;
-			  }
-			});
-
-		}; 
-	   
+	  };	   
 	   
 	   /* CUSTOM NOTIFICATION */
 			
@@ -5056,7 +5024,6 @@ if (typeof NProgress != 'undefined') {
 		init_DataTables();
 		init_chart_doughnut();
 		init_gauge();
-		init_PNotify();
 		init_starrr();
 		init_calendar();
 		init_compose();
